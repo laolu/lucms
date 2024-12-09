17233abf978c84bfba73977b29544a8d8081a737
luzhiwei <lu.zw@qq.com>
2024-12-10 01:21:26 +0800

    数据迁移脚本增加

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,6 +1,17 @@
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
+import { User } from '../users/entities/user.entity';
+import { Content } from '../content/entities/content.entity';
+import { ContentCategory } from '../content/entities/content-category.entity';
+import { ContentAttribute } from '../content/entities/content-attribute.entity';
+import { ContentComment } from '../content/entities/content-comment.entity';
+import { Menu } from '../menu/entities/menu.entity';
+import { Seo } from '../seo/entities/seo.entity';
+import { WebsiteSeo } from '../seo/entities/website-seo.entity';
+import { VipLevel } from '../vip/entities/vip-level.entity';
+import { VipOrder } from '../vip/entities/vip-order.entity';
+import { SystemConfig } from './entities/system-config.entity';
 
 config();
 
@@ -13,7 +24,20 @@ export default new DataSource({
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_DATABASE'),
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  entities: [
+    User,
+    Content,
+    ContentCategory,
+    ContentAttribute,
+    ContentComment,
+    Menu,
+    Seo,
+    WebsiteSeo,
+    VipLevel,
+    VipOrder,
+    SystemConfig
+  ],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   synchronize: false,
+  logging: true,
 }); 
